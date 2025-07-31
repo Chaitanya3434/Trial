1a28857eb45ecf28ba1c8957a92775a837fbd6bb
Chaitanya3434 <sai27283673@gmail.com>
2025-08-01 02:00:15 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title("Medical Reports")
-a=st.text_box("Aadhar No.: ")
+a=st.text_input("Aadhar No.: ")
 x=pd.read_csv(a)
 st.write(x)
 
