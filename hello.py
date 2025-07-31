beebecb6de8c7bdf0f6266c4b41cf06643d94632
Chaitanya3434 <sai27283673@gmail.com>
2025-08-01 01:53:12 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 st.write("Graph")
 x=pd.DataFrame(
-  np.random.randn(234, 4),
-  columns=["a", "b", "c", "d"]
+  np.random.randn(2, 1),
+  columns=["a"]
   
 )
 st.line_chart(x)
