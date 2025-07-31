cf66fdab05a80ed888b206783d8da09cb23a86fe
Chaitanya3434 <sai27283673@gmail.com>
2025-08-01 01:46:22 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,5 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.write("Graph")
-x=pd.DataFrame("Bitcoin Historical Data.csv")
+x=pd.DataFrame(
+  np.random(20, 3)
+)
 st.line_chart(x)
