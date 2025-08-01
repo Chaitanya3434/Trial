89b81e99b260a6392e8b1abc78dbdf156d0690b5
Chaitanya3434 <sai27283673@gmail.com>
2025-08-01 13:01:50 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,9 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title("Medical Reports")
-a=st.file_uploader("Upload File", type={"csv", "txt", "pdf"})
+a=st.file_uploader("Upload File", type={"csv", "txt"})
 if a is not None:
-  x=pd.read_csv(a)
-  st.write(x)
+    x=pd.read_csv(a)
+    st.write(x)
+
 
 
