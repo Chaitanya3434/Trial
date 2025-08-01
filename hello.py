6d2b8a43d89c64bd868a71838234829485a2f415
Chaitanya3434 <sai27283673@gmail.com>
2025-08-01 13:04:07 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title("Medical Reports")
-a=st.file_uploader("Upload File", type={"csv",}
+a=st.file_uploader("Upload File", type={"csv"})
     x=pd.read_csv(a)
     st.write(x)
 
