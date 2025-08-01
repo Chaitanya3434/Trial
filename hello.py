5c0fdf6199a4b6ab01361b46777faa2edd3da957
Chaitanya3434 <sai27283673@gmail.com>
2025-08-01 12:55:48 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 st.title("Medical Reports")
 a=st.file_uploader("Upload File", type={"csv", "txt"})
-if a in not None:
+if a is not None:
   x=pd.read_csv(a)
 st.write(x)
 
